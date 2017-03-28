b2f7d6fd076875767cf1603bd1d9507e42416fb5
Gilbert <PGilbertSchmitt@gmail.com>
2017-03-28 09:20:19 -0700

    Add cell

---
+++ app/src/game/cell.js
@@ -0,0 +1,26 @@
+class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  val() {
+    return `${this.x},${this.y}`;
+  }
+
+  neighbors() {
+    // Entire set of possibilities from moore neighborhood
+    return [
+      `${this.x + 1}, ${this.y + 1}`,
+      `${this.x    }, ${this.y + 1}`,
+      `${this.x - 1}, ${this.y + 1}`,
+      `${this.x - 1}, ${this.y    }`,
+      `${this.x - 1}, ${this.y - 1}`,
+      `${this.x    }, ${this.y - 1}`,
+      `${this.x + 1}, ${this.y - 1}`,
+      `${this.x + 1}, ${this.y    }`
+    ];
+  }
+}
+
+export default Cell;
