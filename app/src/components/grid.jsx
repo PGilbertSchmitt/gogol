bd7afc2e6536badb8787a02cb9a6ade98062819f
Gilbert <PGilbertSchmitt@gmail.com>
2017-03-28 18:02:39 -0700

    Render grid and cells

--- app/src/components/grid.jsx
+++ app/src/components/grid.jsx
@@ -6,23 +6,96 @@ class Grid extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      grid: props.grid
+      grid: props.grid || new Map,
+      centerX: 0,
+      centerY: 0,
+      shiftX: 0,
+      shiftY: 0
     };
-    this.init = this.init.bind(this);
+    this.cellSize = 20;
+    this.draw = this.draw.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentWillReceiveProps(newProps) {
-    this.setState({ grid: newProps.grid });
+    this.setState({ grid: newProps.grid }, this.draw);
+  }
+
+  componentDidMount() {
+    this.canvas = document.getElementById("grid-canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.draw();
   }
 
   draw() {
-    let stage = new createjs.Stage("grid-canvas");
+    // console.log("drawing");
+
+    const viewRect = document.getElementById("grid-canvas").getBoundingClientRect();
+    this.ctx.clearRect(0, 0, viewRect.width, viewRect.height);
+
+    console.log(viewRect);
+    const centerX = viewRect.width / 2 - (this.cellSize / 2 + 1);
+    const centerY = viewRect.height / 2 - (this.cellSize / 2 + 1);
+    this.setState({ centerX, centerY });
+
+    // this.drawLine(centerX, 0, centerX, viewRect.height);
+    for (let i = 0; i < viewRect.width; i += this.cellSize) {
+      this.drawLine(i, 0, i, viewRect.height);
+    }
+
+    for (let i = 0; i < viewRect.width; i += this.cellSize) {
+      this.drawLine(0, i, viewRect.width, i);
+    }
+
+    // console.log(this.state.grid);
+    this.state.grid.forEach(cell => {
+      let x = (cell.x * this.cellSize) + centerX;
+      let y = (cell.y * this.cellSize) + centerY;
+      console.log(`x: ${x}, y: ${y}`);
+      this.drawCell(x, y);
+    });
+  }
+
+  drawCell(x, y) {
+    const ctx = this.ctx;
+
+    ctx.fillStyle = "#101020";
+    ctx.fillRect(x, y, this.cellSize, this.cellSize);
+  }
+
+  drawLine(x1, y1, x2, y2) {
+    const ctx = this.ctx;
+
+    ctx.beginPath();
+    ctx.fillStyle = 'green';
+    ctx.lineWidth = 0;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+  }
+
+  handleClick(e) {
+    let { top, left, width, height } = e.target.getBoundingClientRect();
+    const xOffset = left + (width / 2) - (this.cellSize / 2 + 1);
+    const yOffset = top + (height / 2) - (this.cellSize / 2 + 1);
+
+    let mouseX = Math.floor((e.clientX - xOffset) / this.cellSize);
+    let mouseY = Math.floor((e.clientY - yOffset) / this.cellSize);
+    console.log(`x: ${mouseX}, y: ${mouseY}`);
   }
 
   render() {
     return (
-      <div onLoad={this.draw()}>
-        <canvas id="grid-canvas" width="500" height="300"></canvas>
+      <div
+        onLoad={() => this.draw()}
+        style={{ "padding": "40px" }}>
+        <canvas
+          style={{ "border": "1px solid black" }}
+          id="grid-canvas"
+          width="500"
+          height="300"
+          onMouseDown={this.handleClick}>
+        </canvas>
       </div>
     );
   }
